ec906f39281119a6dab46ab20c05947f208dedd4
Chengyu Huang <chuang@imf.org>
2018-11-09 16:25:35 +0000

    edit setup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,21 @@
 ## install some packages 
-conda install -c conda-forge spacy glob
-conda install gensim nltk numpy pandas jupyter notebook spyder glob2 scikit-learn seaborn
+#conda install -c conda-forge spacy glob
+#conda install gensim nltk numpy pandas jupyter notebook spyder glob2 scikit-learn seaborn
 ## install language module 
-python -m spacy download en_core_web_lg
+#python -m spacy download en_core_web_lg
 
 ## all tho following are automatic now - no need to run 
 ## pip install .tar.gz archive from path or URL
 #pip install /Users/you/en_core_web_sm-2.0.0.tar.gz
 ## set up shortcut link to load installed package as "en_default"
 #python -m spacy link en_core_web_sm en_default
+
+
+mkdir data models 
+cd data 
+mkdir bow_tfidf_docs doc_meta eval frequency processed_json search_terms test_dir train_dir 
+mkdir eval/experts eval/word_groups search_terms/experts
+cd ..
+mkdir models/ngrams models/vsms
 
 
