0a99b119ece153635f43db226f3b6164104e8b17
Aleksander Psurek <apsurek@gmail.com>
2020-05-22 10:31:27 -0400

    Updated country list for smallest test

--- run_all.sh
+++ run_all.sh
@@ -4,7 +4,7 @@ python 02_0_corpus_doc_details.py
 python 02_1_meta_summary.py 
 python 03_corpus_phrases.py 
 python 04_1_vectorize_words.py 
-python 06_frequency_country_specific_freqs.py 
+python 06_0_frequency_country_specific_freqs.py
 
 #python corpus_tfidf.py
 #python frequency_country_specific_freqs.py
