20238beb76ab533a6f6a615391d2ed4dde763458
Aleksander Psurek <apsurek@gmail.com>
2020-06-10 00:54:27 -0400

    Added run of just the 07_02 under two scenarios

---
+++ run_just_07_02.sh
@@ -0,0 +1,5 @@
+source /home/chuange/anaconda3/bin/activate nlp
+cd src_ft
+
+python 07_02_frequency_eval_aggregate.py -gsf final_topic_words_small.csv -tn 10 > ~/logs/log_07_02_1_tt.txt
+python 07_02_frequency_eval_aggregate.py -gsf experts_refined.csv -tn 15 > ~/logs/log_07_02_1_tt.txt
