adfd502f98ec4ba8d79011ead388a445b635bd5a
Aleksander Psurek <apsurek@gmail.com>
2020-06-10 00:55:45 -0400

    Spelling fix in source script

--- run_just_07_02.sh
+++ run_just_07_02.sh
@@ -1,4 +1,4 @@
-source /home/chuange/anaconda3/bin/activate nlp
+source /home/chuang/anaconda3/bin/activate nlp
 cd src_ft
 
 python 07_02_frequency_eval_aggregate.py -gsf final_topic_words_small.csv -tn 10 > ~/logs/log_07_02_1_tt.txt
