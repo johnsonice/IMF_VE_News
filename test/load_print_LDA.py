1d4a5a055e690b633cc08e965aa1b3041abf63d7
Aleksander Psurek <apsurek@gmail.com>
2020-06-18 17:35:33 -0400

    Added test lda file

---
+++ test/load_print_LDA.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.insert(0,'../libs')
+import argparse
+from gensim.models.keyedvectors import KeyedVectors
+#from crisis_points import crisis_points
+from evaluate import evaluate, get_recall, get_precision, get_fscore ,get_input_words_weights,get_country_stats
+import pandas as pd
+#import numpy as np
+import os
+from mp_utils import Mp
+import config
+import gensim
+import argparse
+import config
+import os
+import pandas as pd
+from topic_model_utils import topic2df ##print_topics_gensim
+
+model_folder = "/data/News_data_raw/FT_WD/models/topics"
+this_model = 'lda_model_tfidf_100_None_4'
+model_address = os.path.join(model_folder,this_model)
+loaded_model = gensim.models.ldamodel.LdaModel.load(model_address)
+print("MODEL LOADED at:", model_address)
