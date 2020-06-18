f0801d576cb8fe2017e7949a381aa492fd21eed7
Aleksander Psurek <apsurek@gmail.com>
2020-06-18 17:38:17 -0400

    Fix import libs path

--- test/load_print_LDA.py
+++ test/load_print_LDA.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0,'../libs')
+sys.path.insert(0,'../src_ft/libs')
 import argparse
 from gensim.models.keyedvectors import KeyedVectors
 #from crisis_points import crisis_points
