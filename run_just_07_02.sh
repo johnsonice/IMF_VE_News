acd5b8f0dd256ed2ca0806caa4207817332b4c97
Aleksander Psurek <apsurek@gmail.com>
2020-06-26 15:25:50 -0400

    Changed run_just_07_02.sh to the correct search terms. Added config for it.

--- run_just_07_02.sh
+++ run_just_07_02.sh
@@ -9,14 +9,9 @@ cp config.py config_temp.py
 
 # Run 07_02 with the specific config needed
 rm config.py
-cp config_topic_words.py config.py
+cp config_grouped_words.py config.py
 python config.py
-python 07_02_frequency_eval_aggregate.py -gsf final_topic_words_small.csv -tn 10
-
-rm config.py
-cp config_expert_words.py config.py
-python config.py
-python 07_02_frequency_eval_aggregate.py -gsf experts_refined.csv -tn 15
+python 07_02_frequency_eval_aggregate.py -gsf grouped_search_words_final.csv -tn 15
 
 # Revert to original config
 rm config.py
