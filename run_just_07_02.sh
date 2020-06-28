589e2d54a60490bd709b8d3b26426f8a1b7a8a3e
Aleksander Psurek <apsurek@gmail.com>
2020-06-28 14:09:46 -0400

    Added compare class to 07_02 script

--- run_just_07_02.sh
+++ run_just_07_02.sh
@@ -4,21 +4,7 @@ source /home/chuang/anaconda3/bin/activate nlp
 # Move into right folder
 cd src_ft
 
-# Save current config file
-#cp config.py config_temp.py
-
-# Run 07_02 with the specific config needed
-#rm config.py
-#cp config_grouped_words.py config.py
-#python config.py
-#python 07_02_frequency_eval_aggregate.py -gsf grouped_search_words_final.csv -tn 15
-
-# Run 07_02 with the specific config needed
-#rm config.py
-cp config_grouped_words_just5.py config.py
 python config.py
 python 07_02_frequency_eval_aggregate.py -gsf grouped_search_words_final.csv -tn 15
 
-# Revert to original config
-rm config.py
-cp config_temp.py config.py
+python compare_classification_to_csv.py
