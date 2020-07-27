6212bdd6935891ecfefdcc306cccb80cda8b82a0
Aleksander Psurek <apsurek@gmail.com>
2020-07-27 13:42:58 -0400

    1001 prime 4 run

--- src_ft/10_01_topic_desc_aug_meta.py
+++ src_ft/10_01_topic_desc_aug_meta.py
@@ -8,13 +8,12 @@ if __name__ == "__main__":
     setups = config.class_type_setups
 
     countries = config.countries
-    #topic_f2_thresholds = [('top', 1), ('top', 5), .5, .4, .3]
-    topic_f2_thresholds = [0]
-    #document_topic_min_levels = [("top", 1), ("top", 2), .5, .25, .1, .05]
-    #document_topic_min_levels = [("top", 10), ("top", 20), .02, .01, .05]
-    document_topic_min_levels = [.00001]
+    topic_f2_thresholds = [('top', 1), ('top', 5),('top', 10), .5, .4, .3]
+    #topic_f2_thresholds = [0]
+    document_topic_min_levels = [("top", 1), ("top", 2), .5, .25, .1, .05, ("top", 10), ("top", 20), .02, .01]
+    #document_topic_min_levels = [.00001]
 
-    debug = True  # TEST
+    debug = False  # TEST
 
     '''
     if debug:
