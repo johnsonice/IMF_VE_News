ef8e50a78ec0ad0714482ec980a4aea95399d801
Aleksander Psurek <apsurek@gmail.com>
2020-07-31 12:21:16 -0400

    General clean-up of old files

--- run_from_classification.sh
+++
@@ -1,9 +0,0 @@
-source /home/chuang/anaconda3/bin/activate nlp
-cd src_ft
-python config.py
-
-python 06_0_frequency_country_specific_freqs.py
-python 06_1_export_keywords_timeseries.py
-
-python 07_02_frequency_eval_aggregate.py -gsf grouped_search_words_final.csv -tn 15
-python compare_class_types.py
