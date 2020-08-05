649d3160264598d05dc254ad09df2243b370d829
apsurek <apsurek@gmail.com>
2020-08-05 11:30:53 -0400

    Removed old debug outputs, added search words and outputs of topiccing for comparison

--- run_topic_desc_beg.sh
+++ run_topic_desc_beg.sh
@@ -3,9 +3,9 @@ cd src_ft
 python config.py
 
 # TODO TEMP REMOVEEE
-python 08_07_generate_country_topic_time_series.py
+#python 08_07_generate_country_topic_time_series.py
 
-python 09_04_topic_time_series_eval_aggregate.py
+#python 09_04_topic_time_series_eval_aggregate.py
 
 python 10_01_topic_desc_aug_meta.py
 
