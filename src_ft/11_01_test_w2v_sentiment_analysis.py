a3d8f58e23589ba168fdd98381124956a5814d1a
Aleksander Psurek <apsurek@gmail.com>
2020-08-06 13:11:46 -0400

    11_01 move imported functions into file

--- src_ft/11_01_test_w2v_sentiment_analysis.py
+++ src_ft/11_01_test_w2v_sentiment_analysis.py
@@ -7,8 +7,110 @@ import pandas as pd
 import os
 from mp_utils import Mp
 import config
-from 07_02_frequency_eval_aggregate import read_grouped_search_words, get_sim_words_set, run_evaluation
 
+
+def read_grouped_search_words(file_path):
+    """
+    Read the search words, by group, from the indicated file
+    :param file_path:
+    :return: 'dict' mapping key(group name) -> value(list of str words-in-group)
+    """
+
+    df = pd.read_csv(file_path)
+    search_groups = df.to_dict()
+    for k, v in search_groups.items():
+        temp_list = [i for i in list(v.values()) if not pd.isna(i)]  # Save all non-NA values - different len groups
+        temp_list = [wg.split('&') for wg in temp_list]   # split & for wv search
+        search_groups[k] = temp_list  # Map str key group name -> list[str] group words
+    return search_groups
+
+
+def get_sim_words_set(args,word_group):
+    """
+    Return the top n most similar words based on the word2vec model
+
+    :param args: configuration arguments
+    :param word_group: list[str] of seed words
+    :return: A 'set' of words associated with any of the passed words
+    """
+
+    assert isinstance(word_group,list)  # Must pass a list
+
+    # Iterate over list
+    sim_word_group = list()
+    for w in word_group:
+
+        # Save associated top n words, weights - if exist in the word2vec model
+        try:
+            words, weights = get_input_words_weights(args,
+                                                 w,
+                                                 vecs=vecs,
+                                                 weighted=args.weighted)
+            sim_word_group.extend(words)
+
+        # If a word does not exist in the word2vec model, alert and ignore
+        except:
+            print('Not in vocabulary {}'.format(w))
+    sim_word_set = set(sim_word_group)  # Eliminate duplicate associated words for group
+
+    return sim_word_set
+
+
+def run_evaluation(item,args,weights=None): # TODO clean up naming practice - wth is "item"
+    """
+    TODO docstring
+    :param item:
+    :param args:
+    :param weights:
+    :param export:
+    :return:
+    """
+    # use topn most similar terms as words for aggregate freq if args.sims
+    # get dataframe of evaluation metrics for each indivicual country
+    k, words = item # TODO how is words defined
+
+    # Get evaluation stats per country
+    all_stats = get_country_stats(args.countries, words,
+                                  args.frequency_path,
+                                  args.window,
+                                  args.months_prior,
+                                  args.method,
+                                  args.crisis_defs,
+                                  period=args.period,
+                                  eval_end_date=args.eval_end_date,
+                                  weights=weights,
+                                  z_thresh=args.z_thresh)
+
+    # Aggregate tp, fp, fn numbers for all countries to calc overall eval metrics
+    aggregate_tp, aggregate_fp, aggregate_fn = all_stats['tp'].sum(), all_stats['fp'].sum(), all_stats['fn'].sum()
+    aggregate_recall = get_recall(aggregate_tp, aggregate_fn)
+    aggregate_prec = get_precision(aggregate_tp, aggregate_fp)
+    aggregate_f2 = get_fscore(aggregate_tp, aggregate_fp, aggregate_fn, beta=2)
+    aggregate_stats = pd.Series([aggregate_recall, aggregate_prec, aggregate_f2, aggregate_tp, aggregate_fp, aggregate_fn],
+                    name='aggregate',
+                    index=['recall','precision','fscore','tp','fp','fn'])
+    all_stats = all_stats.append(aggregate_stats)
+
+    # Save to file if export
+    if args.export:
+        all_stats.to_csv(os.path.join(args.eval_path,
+                                      'agg_sim_{}_{}_offset_{}_smoothwindow_{}_{}_evaluation.csv'.format(args.sims,
+                                                                                                       args.period,
+                                                                                                       args.months_prior,
+                                                                                                       args.window,
+                                                                                                       k)))
+
+    # Print results if verbose
+    if args.verbose:
+        print('\n\n{}:\nevaluated words: {}\n\trecall: {}, precision: {}, f-score: {}'.format(k,words,aggregate_recall, aggregate_prec, aggregate_f2))
+    if args.weighted:
+        return k,list(zip(words,weights)),aggregate_recall, aggregate_prec, aggregate_f2
+    else:
+        return k,words,aggregate_recall, aggregate_prec, aggregate_f2
+    #print('evaluated words: {}'.format(words))
+
+
+# TODO kick all the stuff out of above main and import it from 07_02, or move out of that one and create a utils file
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
@@ -31,7 +133,7 @@ if __name__ == "__main__":
     groups = read_grouped_search_words(args.search_file)
 
     args.verbose = True  # Todo modularize
-    args.export = True  # TODO modularize
+    args.export = False  # TODO modularize
     args.eval_path = config.EXP_SEARCH_EVAL
     args.frequency_path = os.path.join(config.NEW_PROCESSING_FOLDER, 'frequency', 'csv', 'Min1_AllCountry') # TEMP
 
