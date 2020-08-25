ff8e4a97b027af12480d0f572a11d40d7d4d5899
Aleksander Psurek <apsurek@gmail.com>
2020-08-25 18:22:45 -0400

    Fix w2v exp script argparse outputs

--- src_ft/w2v_size_exp.py
+++ src_ft/w2v_size_exp.py
@@ -7,11 +7,13 @@ from subprocess import Popen  # Allows to run other python files
 
 sample_size = 50000  # Test
 # sample_size = 500000
-window_sizes = [5, 10, 15, 20]
+window_sizes = [5]
+#window_sizes = [5, 10, 15, 20]
 models_directory = '/data/News_data_raw/FT_WD/models/vsms_test'
 
 for window_size in window_sizes:
-    call_list = ['python', '04_1_vectorize_words.py', '-wd', window_size, '-spz', sample_size, '-out', models_directory]
+    call_list = ['python', '04_1_vectorize_words.py', '-wd', str(window_size), '-spz', str(sample_size),
+                 '-out', models_directory]
     # Run CBOW
     child = Popen(call_list)
     child.wait()
