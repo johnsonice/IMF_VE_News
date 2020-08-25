7b4128f73a3cf6fd45635a355133e363c2d16f03
Aleksander Psurek <apsurek@gmail.com>
2020-08-25 18:27:49 -0400

    w2v exp to whole sub-part and modes

--- src_ft/w2v_size_exp.py
+++ src_ft/w2v_size_exp.py
@@ -5,10 +5,10 @@ sys.path.insert(0, './libs')
 import config
 from subprocess import Popen  # Allows to run other python files
 
-sample_size = 50000  # Test
-# sample_size = 500000
-window_sizes = [5]
-#window_sizes = [5, 10, 15, 20]
+#sample_size = 50000  # Test
+sample_size = 500000
+#window_sizes = [5]
+window_sizes = [5, 10, 15, 20]
 models_directory = '/data/News_data_raw/FT_WD/models/vsms_test'
 
 for window_size in window_sizes:
