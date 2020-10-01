bfd89bb7387fd500663ac8dd2dfc64e7d3e06ad3
Aleksander Psurek <apsurek@gmail.com>
2020-10-01 06:08:19 -0400

    Fix to country fetching

--- src_ft/07_02_frequency_eval_aggregate.py
+++ src_ft/07_02_frequency_eval_aggregate.py
@@ -231,7 +231,7 @@ if __name__ == '__main__':
                 args.eval_path = ev_path
 
                 args.crisis_defs = asses_type
-                args.countries = list(args.crisis_defs.keys())
+                args.countries = list(assess_dict[asses_type].keys())
 
                 # Execute the process setups times
                 main_process(args, iter_items)
