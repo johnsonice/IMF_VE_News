79c0cba46033f33a641ca8bac4d9664da64becf5
Aleksander Psurek <apsurek@gmail.com>
2020-10-15 13:15:32 -0400

    Fix get_sim_words, account for jaggedness

--- src_ft/temp/get_associated_sim_word.py
+++ src_ft/temp/get_associated_sim_word.py
@@ -4,6 +4,7 @@ sys.path.insert(0,'../libs')
 import pandas as pd
 import config
 from gensim.models.keyedvectors import KeyedVectors
+import numpy as np
 
 
 def get_sim_words(vecs,wg,topn):
@@ -30,8 +31,17 @@ vecs = KeyedVectors.load(config.W2V)
 targets = config.targets
 target_sim_dict = {}
 
+lengths = []
 for target in targets:
     target_sim_dict[target] = get_sim_words(vecs, target, 15)
+    lengths.append(len(target_sim_dict[target]))
+
+max_len = max(lengths)
+
+for target in targets:
+    if len(target_sim_dict[target]) < max_len:
+        missing_len = max_len - len(target_sim_dict[target])
+        target_sim_dict[target] = target_sim_dict[target] + [np.NaN]*missing_len
 
 target_sim_df = pd.DataFrame(target_sim_dict)
 
