e0d144c9b2f0c751a2afe5bc495af42e3151a0ea
Aleksander Psurek <apsurek@gmail.com>
2020-11-02 05:46:03 -0500

    Added negation and total word counting

--- src_ft/temp/non_vector_freqs.py
+++ src_ft/temp/non_vector_freqs.py
@@ -4,7 +4,7 @@ sys.path.insert(0,'../libs')
 import pandas as pd
 import config
 import crisis_points
-from frequency_utils import rolling_z_score, aggregate_freq, signif_change
+from frequency_utils import rolling_z_score, aggregate_freq, signif_change, all_word_count
 from mp_utils import Mp
 
 
@@ -24,6 +24,8 @@ if __name__ == "__main__":
     positive_targs = ['able', 'enable', 'adequately', 'benign', 'buoyant', 'buoyancy', 'comfortable', 'confident', 'enhance', 'favorable', 'favourably', 'healthy', 'improve', 'improvement', 'mitigate', 'positive', 'positively', 'profits', 'rebound', 'recover', 'recovery', 'resilience', 'resilient', 'solid', 'sound', 'stabilise', 'stabilize', 'success', 'successful', 'successfully']
     negative_targs = ['abrupt', 'adverse', 'adversely', 'aggravate', 'bad', 'burden', 'challenge', 'closure', 'contraction', 'costly', 'damage', 'danger', 'deficit', 'dent', 'destabilise', 'deteriorate', 'deterioration', 'deterioration', 'difficult', 'discourage', 'downgrade', 'drag', 'erode', 'erosion', 'exacerbate', 'expose', 'fear', 'force', 'fragility', 'gloomy', 'hurt', 'illiquid', 'impairment', 'inability', 'jeopardise', 'lose', 'negative', 'pose', 'question', 'repercussion', 'risky', 'severely', 'shortfall', 'spiral', 'squeeze', 'stagnate', 'strain', 'stress', 'struggle', 'suffer', 'threaten', 'turbulent', 'unable', 'undermine', 'unease', 'unexpectedly', 'vulnerable', 'weakness', 'worsen', 'writedowns']
     non_vec_targs = list(set(positive_targs + negative_targs)) # Should be irrelevant
+
+    negation_targ = ['negation']
 
     def fetch_list(word):
         return list(words_to_lists_df[word].dropna())
@@ -45,5 +47,39 @@ if __name__ == "__main__":
 
         return country, df_all
 
+    def export_country_negative_count(country, period=period, frequency_path=frequency_path, out_dir=out_dir):
+        series_wg = list()
+        negation_list = ['aint', 'cannot', "can't", "daren't", "didn't", "doesn't", "don't", "hadn't", 'hardly',
+                         "hasn't", "haven't", "havn't", "isn't", 'lack', 'lacking', 'lacks', 'neither', 'never',
+                         'no', 'nobody', 'none', 'nor', 'not', 'nothing', 'nowhere', "mightn't", "mustn't", "needn't",
+                         "oughtn't", "shan't", "shouldn't", "wasn't", "without", "wouldn't"]
+
+        df = aggregate_freq(negation_list, country, period=period, stemmed=False, frequency_path=frequency_path)
+        df.name = 'negation_index'
+        series_wg.append(df)
+
+        df_all = pd.concat(series_wg, axis=1)
+        #out_csv = os.path.join(out_dir, '{}_{}_time_series_non_vec.csv'.format(country, period))
+        #out_csv = os.path.join(out_dir, '{}_{}_time_series_cherry_picked.csv'.format(country, period))
+        out_csv = os.path.join(out_dir, '{}_{}_negation.csv'.format(country, period))
+        df_all.to_csv(out_csv)
+
+        return country, df_all
+
+    def get_word_counts(country, period=period, frequency_path=frequency_path, out_dir=out_dir):
+        series_wg = list()
+        df = all_word_count(country, period=period, stemmed=False, frequency_path=frequency_path)
+        df.name = 'total_word_count'
+        series_wg.append(df)
+
+        df_all = pd.concat(series_wg, axis=1)
+        #out_csv = os.path.join(out_dir, '{}_{}_time_series_non_vec.csv'.format(country, period))
+        #out_csv = os.path.join(out_dir, '{}_{}_time_series_cherry_picked.csv'.format(country, period))
+        out_csv = os.path.join(out_dir, '{}_{}_word_counts.csv'.format(country, period))
+        df_all.to_csv(out_csv)
+
+        return country, df_all
+
     for country in countries:
-        export_country_ts(country)
+        export_country_negative_count(country)
+        get_word_counts(country)
